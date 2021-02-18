8f8c5fe0cd8671824aecf66ec80fccb05be9ddd2
jamie <jbr43@cam.ac.uk>
2021-02-18 17:01:38 +0000

    made it easier to switch between settings for final or synthetic plotting

--- plot.py
+++ plot.py
@@ -5,6 +5,27 @@ import numpy as np
 from glob import glob
 
 
+class Settings:
+    def __init__(self, mode):
+        if mode == 'synth-iso':
+            self.skip = 2
+            self.scale = 7
+            self.draw_iso = True
+            self.draw_aniso = False
+        elif mode == 'synth-aniso':
+            self.skip = 2
+            self.scale = 7
+            self.draw_iso = False
+            self.draw_aniso = True
+        elif mode == 'final':
+            self.skip = 1
+            self.scale = 2.54
+            self.draw_iso = True
+            self.draw_aniso = True
+        else:
+            raise ValueError
+
+
 def scalar_prepare(df):
     # df.Strength = (df.Strength - df.Strength.min()) / (df.Strength.max() - df.Strength.min())
     Z = df.pivot_table(index='Longitude', columns='Latitude', values='Strength').T.values
@@ -26,21 +47,40 @@ def vector_prepare(df):
 
     # normalise
     # U = (U - U.min()) / (U.range())
-    return X, Y, U, V
+    return np.array([X, Y, U, V])
 
 
 def main():
-    files = glob('data/xyz/*.xyz')
+    settings = Settings('synth-aniso')
+
+    def plot_iso():
+        path = glob('data/xyz/*_v_*.xyz')[0]
+        iso_v = pd.read_csv(path, header=None, delim_whitespace=True, names=['Latitude', 'Longitude', 'Strength'])
+        iso_v = scalar_prepare(iso_v)
+        filled_c = ax.contourf(*iso_v, 200, transform=proj, cmap='seismic_r')
+        return filled_c
+
+    def plot_aniso():
+        path = glob('data/xyz/*_an2v_*.xyz')[0]
+
+        ans_2v = pd.read_csv(path, header=None, delim_whitespace=True,
+                             names=['Latitude', 'Longitude', 'Azimuth', 'Strength'])
+        ans_2v = vector_prepare(ans_2v)
+
+        skip = slice(None, None, settings.skip)
+        ans_2v = ans_2v[:, skip]  # reduce vector density
+
+        aniso = ax.quiver(*ans_2v, transform=proj, pivot='mid', headlength=0, headaxislength=0, units='inches',
+                          scale=settings.scale,  # 2.54 for 1 cm : 100 %
+                          scale_units='inches')
+        quivkey = ax.quiverkey(aniso, X=0.5, Y=-0.1, U=1, label='2% peak-to-peak anisotropy')
+        return aniso, quivkey
+
     # stations = pd.read_csv('data/stations.csv', index_col=0)
 
     # pi anisotropy
-    ans_2v = pd.read_csv(files[2], header=None, delim_whitespace=True,
-                         names=['Latitude', 'Longitude', 'Azimuth', 'Strength'])
-    # isotropic speed
-    iso_v = pd.read_csv(files[0], header=None, delim_whitespace=True, names=['Latitude', 'Longitude', 'Strength'])
 
-    iso_v = scalar_prepare(iso_v)
-    ans_2v = vector_prepare(ans_2v)
+    # isotropic speed
 
     proj = ccrs.PlateCarree()
     fig = plt.figure(figsize=(13, 10))
@@ -51,11 +91,18 @@ def main():
 
     ax.set_extent(boundingbox)
 
-    filled_c = ax.contourf(*iso_v, 200, transform=proj, cmap='seismic_r')
+    if settings.draw_iso:
+        filled_c = plot_iso()
+
+        cax = fig.add_axes([ax.get_position().x1 + 0.05, ax.get_position().y0, 0.02, ax.get_position().height])
+        fig.colorbar(filled_c, orientation='vertical', cax=cax)
+
+    if settings.draw_aniso:
+        aniso, quivkey = plot_aniso()
+
     # line_c = ax.contour(*iso_v, levels=4, colors=['black'],transform=ccrs.Miller())
     # ax.scatter(iso_v.Latitude.to_numpy(), iso_v.Longitude.to_numpy(), c=iso_v.Strength.to_numpy())
-    aniso = ax.quiver(*ans_2v, transform=proj, pivot='mid', headlength=0, headaxislength=0, units='inches', scale=2.54, scale_units='inches')
-    ax.quiverkey(aniso, X=0.3, Y=0.9, U=1, label='2% peak-to-peak anisotropy')
+
     # sta = ax.scatter(stations.Longitude, stations.Latitude)
     ax.coastlines()
     ax.gridlines(draw_labels=True)
@@ -63,9 +110,6 @@ def main():
     # ax.add_feature(cfeature.OCEAN)
     # ax.stock_img()
     # ax.set_global()
-
-    cax = fig.add_axes([ax.get_position().x1 + 0.05, ax.get_position().y0, 0.02, ax.get_position().height])
-    fig.colorbar(filled_c, orientation='vertical', cax=cax)
 
     plt.show()
 
